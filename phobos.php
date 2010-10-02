1f54d324be24590d54c5049120968c73b933c1bd
Adrian <devnull@localhost>
2010-10-02 21:05:51 +0000

    fixed seen reporting last seen user

--- phobos.php
+++ phobos.php
@@ -508,9 +508,9 @@ class Phobos extends Nexus {
 					if (!$seen_found) {
 						foreach ($this->seen_list as $key => $val) {
 							if ($this->iswm($tmp_seenwho,$key,$strict = false)) {
-								$this->send("PRIVMSG $chan :last seen $key ".($val['host'] ? "(".$val['host'].")":"")." ".
-											$this->seen_list[$key]['action'])." ".
-											$this->duration($this->seen_list[$key]['time'])." ago";
+								$this->send("PRIVMSG $chan :last seen $key ".
+											($val['host'] ? "(".$val['host'].")":"")." ".$val['action']." ".
+											$this->duration($val['time'])." ago";
 								$seen_found = true;
 								break;
 							}
